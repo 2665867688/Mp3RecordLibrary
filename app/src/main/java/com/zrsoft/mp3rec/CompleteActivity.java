45a27329cef422ffa564597340ede3f7584b35f7
shimy <2665867688@qq.com>
2017-08-08 15:54:22 +0800

    MP3录音功能

--- app/src/main/java/com/zrsoft/mp3rec/CompleteActivity.java
+++ app/src/main/java/com/zrsoft/mp3rec/CompleteActivity.java
@@ -1,7 +1,10 @@
 package com.zrsoft.mp3rec;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class CompleteActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class CompleteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_complete);
+        findViewById(R.id.btn_record).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(CompleteActivity.this,GoRecordActivity.class));
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
---
+++ app/src/main/java/com/zrsoft/mp3rec/GoRecordActivity.java
@@ -0,0 +1,28 @@
+package com.zrsoft.mp3rec;
+
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+/**
+ * 
+ * @ClassName:GoRecordActivity
+ * @author: shimy
+ * @date: 2017/8/8 0008 下午 3:47
+ * @description: 录音
+ */
+public class GoRecordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_go_record);
+    }
+
+
+
+}
