e990b590aaeef182841ecf3d6e656ca8682a98cf
shimy <2665867688@qq.com>
2017-08-09 11:25:30 +0800

    在服务中录音，加入前台通知

---
+++ app/src/main/java/com/zrsoft/mp3rec/utils/OutCallRecordActivity.java
@@ -0,0 +1,22 @@
+package com.zrsoft.mp3rec.utils;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.zrsoft.mp3rec.R;
+
+/**
+ *
+ * @ClassName:OutCallRecordActivity
+ * @author: shimy
+ * @date: 2017/8/9 0009 上午 11:23
+ * @description: 供外部调用的录音
+ */
+public class OutCallRecordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_out_call_record);
+    }
+}
