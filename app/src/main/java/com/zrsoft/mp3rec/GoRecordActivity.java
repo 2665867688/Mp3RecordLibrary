7114e991aa85f246f00d742013640473c7e04052
shimy <2665867688@qq.com>
2017-08-09 10:41:41 +0800

    在服务中录音，加入前台通知

--- app/src/main/java/com/zrsoft/mp3rec/GoRecordActivity.java
+++ app/src/main/java/com/zrsoft/mp3rec/GoRecordActivity.java
@@ -1,21 +1,34 @@
 package com.zrsoft.mp3rec;
 
+import android.Manifest;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Handler;
+import android.os.IBinder;
 import android.os.Message;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.zrsoft.mp3rec.lame.MP3Recorder;
 import com.zrsoft.mp3rec.utils.RecorderAndPlayUtil;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * @ClassName:GoRecordActivity
@@ -23,28 +36,49 @@ import java.util.ArrayList;
  * @date: 2017/8/8 0008 下午 3:47
  * @description: 录音
  */
-public class GoRecordActivity extends AppCompatActivity implements View.OnClickListener{
+public class GoRecordActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button btnControl;
     private Button btnSave;
     private Button btnGiveUp;
+    private TextView tvTimeShow;
     private Uri uri;
-    private RecorderAndPlayUtil mRecorder = null;
     private int state = 0;//0：开始 1：播放中 2：暂停
+
+    private RecordService.RecordBinder binder;
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            binder = (RecordService.RecordBinder) service;
+            //向service设置回调接口，service发送通知到activity更新界面
+            binder.getRecordService().setOnRecordListener(onRecordListener);
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_go_record);
-        mRecorder = new RecorderAndPlayUtil(this,uri);
-        mRecorder.getRecorder().setHandle(handler);
-
         uri = getIntent().getParcelableExtra(MediaStore.EXTRA_OUTPUT);
+
         btnControl = (Button) findViewById(R.id.btn_control);
         btnControl.setOnClickListener(this);
         btnSave = (Button) findViewById(R.id.btn_save);
         btnSave.setOnClickListener(this);
         btnGiveUp = (Button) findViewById(R.id.btn_give_up);
         btnGiveUp.setOnClickListener(this);
+        tvTimeShow = (TextView) findViewById(R.id.tv_show_time);
+        Intent intentService = new Intent(this,RecordService.class);
+        intentService.putExtra(MediaStore.EXTRA_OUTPUT,uri);
+        startService(intentService);
+        bindService(intentService, connection, BIND_AUTO_CREATE); // 绑定服务
+        if (ContextCompat.checkSelfPermission(GoRecordActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(GoRecordActivity.this, new String[]{ Manifest.permission. WRITE_EXTERNAL_STORAGE }, 1);
+        }
     }
 
 
@@ -52,87 +86,58 @@ public class GoRecordActivity extends AppCompatActivity implements View.OnClickL
     public void onClick(View v) {
         int i = v.getId();
         if (i == R.id.btn_control) {
-            if (state ==0){
-                mRecorder.startRecording();
+            if (state == 0) {
+                binder.startRecord();
                 btnControl.setText("暂停");
                 state = 1;
-            }else if (state == 1){
-                mRecorder.pauseRecording();
+            } else if (state == 1) {
+                binder.pauseRecord();
                 btnControl.setText("继续");
                 state = 2;
-            }else if (state ==2){
-                mRecorder.restoreRecording();
+            } else if (state == 2) {
+                binder.restoreRecord();
                 btnControl.setText("暂停");
                 state = 1;
             }
         } else if (i == R.id.btn_save) {
-
-        } else if (i == R.id.btn_give_up){
-            finish();
+            //保存录音 取消前台通知
+            binder.saveRecord();
+            Toast.makeText(this, "舍弃录音", Toast.LENGTH_SHORT).show();
+        } else if (i == R.id.btn_give_up) {
+            //舍弃 将录音文件删除掉 取消前台通知
+            binder.giveUp();
+            btnControl.setText("开始");
+            tvTimeShow.setText("录音");
         }
     }
 
+
+    RecordService.OnRecordListener onRecordListener = new RecordService.OnRecordListener() {
+        @Override
+        public void recordTime(String time) {
+            tvTimeShow.setText(time);
+        }
+    };
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
 
         return super.onKeyDown(keyCode, event);
     }
-    private Handler handler = new Handler(){
-        @Override
-        public void handleMessage(Message msg) {
-            switch (msg.what) {
-                case MP3Recorder.MSG_REC_STARTED:
-                    // 开始录音
-                    Toast.makeText(getApplicationContext(), "开始录音",
-                            Toast.LENGTH_SHORT).show();
-                    break;
-                case MP3Recorder.MSG_REC_STOPPED:
-                    //停止录音 录音保存
-                    Toast.makeText(getApplicationContext(), "停止录音",
-                            Toast.LENGTH_SHORT).show();
-                    break;
-                case MP3Recorder.MSG_ERROR_GET_MIN_BUFFERSIZE:
-                    initRecording();
-                    Toast.makeText(getApplicationContext(), "采样率手机不支持",
-                            Toast.LENGTH_SHORT).show();
-                    break;
-                case MP3Recorder.MSG_ERROR_CREATE_FILE:
-                    initRecording();
-                    Toast.makeText(getApplicationContext(), "创建音频文件出错",
-                            Toast.LENGTH_SHORT).show();
-                    break;
-                case MP3Recorder.MSG_ERROR_REC_START:
-                    initRecording();
-                    Toast.makeText(getApplicationContext(), "初始化录音器出错",
-                            Toast.LENGTH_SHORT).show();
-                    break;
-                case MP3Recorder.MSG_ERROR_AUDIO_RECORD:
-                    initRecording();
-                    Toast.makeText(getApplicationContext(), "录音的时候出错",
-                            Toast.LENGTH_SHORT).show();
-                    break;
-                case MP3Recorder.MSG_ERROR_AUDIO_ENCODE:
-                    initRecording();
-                    Toast.makeText(getApplicationContext(), "编码出错",
-                            Toast.LENGTH_SHORT).show();
-                    break;
-                case MP3Recorder.MSG_ERROR_WRITE_FILE:
-                    initRecording();
-                    Toast.makeText(getApplicationContext(), "文件写入出错",
-                            Toast.LENGTH_SHORT).show();
-                    break;
-                case MP3Recorder.MSG_ERROR_CLOSE_FILE:
-                    initRecording();
-                    Toast.makeText(getApplicationContext(), "文件流关闭出错",
-                            Toast.LENGTH_SHORT).show();
-                    break;
-            }
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case 1:
+                if (grantResults.length > 0 && grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "拒绝权限将无法使用程序", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                break;
+            default:
         }
-    };
-
-
-    private void initRecording() {
-        mRecorder.stopRecording();
     }
+
 }
---
+++ app/src/main/java/com/zrsoft/mp3rec/RecordService.java
@@ -0,0 +1,203 @@
+package com.zrsoft.mp3rec;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Binder;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.provider.MediaStore;
+import android.support.annotation.IntDef;
+import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
+import android.widget.Toast;
+
+import com.zrsoft.mp3rec.lame.MP3Recorder;
+import com.zrsoft.mp3rec.utils.RecorderAndPlayUtil;
+
+import java.io.File;
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * @ClassName:录音server
+ * @author: shimy
+ * @date: 2017/8/9 0009 上午 8:41
+ * @description:
+ */
+public class RecordService extends Service {
+
+    private RecordBinder binder = new RecordBinder();
+    private final static int MSG_TIME = 102;
+    private long time;
+    private Uri uri;
+    private RecorderAndPlayUtil mRecorder = null;
+
+
+    public RecordService() {
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        uri = intent.getParcelableExtra(MediaStore.EXTRA_OUTPUT);
+        mRecorder = new RecorderAndPlayUtil(this, uri);
+        mRecorder.getRecorder().setHandle(handler);
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return binder;
+    }
+
+    class RecordBinder extends Binder {
+        public void startRecord() {
+            mRecorder.startRecording();
+            timer.schedule(new RecTimerTask(), 0, 1000);
+            startForeground(1, getNotification("录音", 0));
+        }
+
+        public void pauseRecord() {
+            mRecorder.pauseRecording();
+            timer.cancel();
+        }
+
+        public void restoreRecord() {
+            mRecorder.restoreRecording();
+            timer = new Timer();
+            timer.schedule(new RecTimerTask(), 0, 1000);
+        }
+
+        public void saveRecord() {
+            //保存录音 取消前台通知
+            timer.cancel();
+            mRecorder.stopRecording();//停止并保存录音
+            getNotificationManager().cancel(1);
+            stopForeground(true);
+        }
+
+        public void giveUp() {
+            //舍弃 将录音文件删除掉 取消前台通知
+            File file = new File(mRecorder.getRecorderPath());
+            file.delete();
+            getNotificationManager().cancel(1);
+            stopForeground(true);
+        }
+
+        public RecordService getRecordService() {
+            return RecordService.this;
+        }
+    }
+
+    private NotificationManager getNotificationManager() {
+        return (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+    }
+
+    private Notification getNotification(String title, long timer) {
+        Intent intent = new Intent(this, GoRecordActivity.class);
+        PendingIntent pi = PendingIntent.getActivity(this, 0, intent, 0);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher));
+        builder.setContentIntent(pi);
+        builder.setContentTitle("录音");
+        builder.setContentText(timer + "");
+        return builder.build();
+    }
+
+    Timer timer = new Timer();
+
+    class RecTimerTask extends TimerTask {
+
+        @Override
+        public void run() {
+            // 需要做的事:发送消息
+            handler.sendEmptyMessage(MSG_TIME);
+        }
+    }
+
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MSG_TIME://时间改变
+                    time++;
+                    onRecordListener.recordTime(time + "");
+                    getNotificationManager().notify(1,getNotification("录音",time));
+                    break;
+                case MP3Recorder.MSG_REC_STARTED:
+                    // 开始录音
+                    Toast.makeText(getApplicationContext(), "开始录音",
+                            Toast.LENGTH_SHORT).show();
+                    break;
+                case MP3Recorder.MSG_REC_STOPPED:
+                    //停止录音 录音保存
+                    Toast.makeText(getApplicationContext(), "停止录音",
+                            Toast.LENGTH_SHORT).show();
+                    break;
+                case MP3Recorder.MSG_ERROR_GET_MIN_BUFFERSIZE:
+                    initRecording();
+                    Toast.makeText(getApplicationContext(), "采样率手机不支持",
+                            Toast.LENGTH_SHORT).show();
+                    break;
+                case MP3Recorder.MSG_ERROR_CREATE_FILE:
+                    initRecording();
+                    Toast.makeText(getApplicationContext(), "创建音频文件出错",
+                            Toast.LENGTH_SHORT).show();
+                    break;
+                case MP3Recorder.MSG_ERROR_REC_START:
+                    initRecording();
+                    Toast.makeText(getApplicationContext(), "初始化录音器出错",
+                            Toast.LENGTH_SHORT).show();
+                    break;
+                case MP3Recorder.MSG_ERROR_AUDIO_RECORD:
+                    initRecording();
+                    Toast.makeText(getApplicationContext(), "录音的时候出错",
+                            Toast.LENGTH_SHORT).show();
+                    break;
+                case MP3Recorder.MSG_ERROR_AUDIO_ENCODE:
+                    initRecording();
+                    Toast.makeText(getApplicationContext(), "编码出错",
+                            Toast.LENGTH_SHORT).show();
+                    break;
+                case MP3Recorder.MSG_ERROR_WRITE_FILE:
+                    initRecording();
+                    Toast.makeText(getApplicationContext(), "文件写入出错",
+                            Toast.LENGTH_SHORT).show();
+                    break;
+                case MP3Recorder.MSG_ERROR_CLOSE_FILE:
+                    initRecording();
+                    Toast.makeText(getApplicationContext(), "文件流关闭出错",
+                            Toast.LENGTH_SHORT).show();
+                    break;
+            }
+        }
+    };
+
+
+    private void initRecording() {
+        mRecorder.stopRecording();
+    }
+
+    private OnRecordListener onRecordListener;
+
+    public void setOnRecordListener(OnRecordListener listener) {
+        this.onRecordListener = listener;
+    }
+
+    public interface OnRecordListener {
+        void recordTime(String time);
+    }
+}
