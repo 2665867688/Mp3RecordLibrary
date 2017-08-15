b36e2f08a58427800586a94c6c6d95d5cd79a68a
shimy <2665867688@qq.com>
2017-08-15 08:03:31 +0800

    dialog弹出选择保存状态

---
+++ app/src/main/java/com/zrsoft/mp3rec/ripple/RippleTestActivity.java
@@ -0,0 +1,21 @@
+package com.zrsoft.mp3rec.ripple;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.zrsoft.mp3rec.R;
+/**
+ * 
+ * @ClassName:RippleTestActivity
+ * @author: shimy
+ * @date: 2017/8/10 0010 下午 1:27
+ * @description: 波纹效果
+ */
+public class RippleTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ripple_test);
+    }
+}
